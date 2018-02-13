52d0ad8e26c43beb339a0f62e463067a72265578
lvrub <leon_lm@i.ua>
2018-02-13 23:32:26 +0200

    unitTest for BadStudent and TeacherForStudent classes
    #5

--- src/test/java/net/commments/sample/AttentetiveStudentTest.java
+++ src/test/java/net/commments/sample/AttentetiveStudentTest.java
@@ -7,7 +7,6 @@ import org.testng.annotations.Test;
 public class AttentetiveStudentTest {
     @Test
     public void checkAttentiveSudentName() {
-
         AttentetiveStudent student = new AttentetiveStudent("leonid");
         String name = student.name();
         MatcherAssert.assertThat(name, Matchers.is("leonid"));
