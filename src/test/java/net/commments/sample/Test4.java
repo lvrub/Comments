f996e7c8a73475dca6c5aa5d34ec35de79ce967a
lvrub <leon_lm@i.ua>
2018-02-17 13:44:05 +0200

    Copy automated test #4, setup it for Chrome
    #8

---
+++ src/test/java/net/commments/sample/Test4.java
@@ -0,0 +1,44 @@
+package net.commments.sample;
+
+import org.hamcrest.MatcherAssert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * @author Dmytro Serdiuk (dmytro.serdiuk@gmail.com)
+ * @version $Id$
+ * @since ?????
+ */
+public class Test4 {
+
+    private final WebDriver[] driver = new WebDriver[1];
+
+    @Test
+    public void test() {
+        this.driver().get("http://commentssprintone.azurewebsites.net");
+        this.driver().findElements(By.name("SelectedId")).get(0).click();
+        this.driver().findElement(By.xpath("//*[@value=\"Delete\"]")).click();
+        this.driver().findElement(By.xpath("//span[text()=\"Yes\"]")).click();
+        final String page = this.driver().getPageSource();
+        MatcherAssert.assertThat("Comment Text 0 is present", !page.contains("Comment Text 0"));
+    }
+
+    @BeforeMethod
+    public void createDriver() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Automation\\src\\main\\resources\\chromedriver.exe");
+        driver[0] = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        driver[0].close();
+    }
+
+    private WebDriver driver() {
+        return this.driver[0];
+    }
+}
