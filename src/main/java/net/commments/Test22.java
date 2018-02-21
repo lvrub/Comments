aeedb70868b2f72949c9a9a4d6113be9b990cb23
lvrub <leon_lm@i.ua>
2018-02-21 19:53:26 +0200

    21-23 lines are joined into one.
    #13

--- src/main/java/net/commments/Test22.java
+++ src/main/java/net/commments/Test22.java
@@ -2,7 +2,6 @@ package net.commments;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -18,9 +17,7 @@ public class Test22 {
     public void test() {
         this.driver().get("http://commentssprintone.azurewebsites.net");
         this.driver().findElements(By.id("newbutton")).get(0).click();
-        WebElement text = this.driver().findElement(By.id("Text"));
-        String symbols = "@#$%^&*&*";
-        text.sendKeys(symbols);
+        this.driver().findElement(By.id("Text")).sendKeys("@#$%^&*&*");
         this.driver().findElements(By.xpath("//*[@id=\"editor-navigation\"]/input[1]")).get(0).click();
         String errorText = this.driver().findElement(By.id("errorfield")).getText();
         assertEquals(errorText, "The Comment Text field should contain alphanumeric characters only");
