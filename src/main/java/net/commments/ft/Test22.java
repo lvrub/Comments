d6b336e6c5fd522bac00261949ae912d37adf88a
lvrub <leon_lm@i.ua>
2018-02-24 14:38:51 +0200

    Remove extending of BaseDrived in Test#22
    #15

--- src/main/java/net/commments/ft/Test22.java
+++ src/main/java/net/commments/ft/Test22.java
@@ -4,18 +4,20 @@ import net.commments.object.BCommentPage;
 import net.commments.object.BCommentWindow;
 import net.commments.object.CommentPage;
 import net.commments.object.CommentWindow;
+import net.commments.selenium.CommentsDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-public class Test22 extends BaseDriver {
-
-
+public class Test22 {
+    protected final CommentsDriver driver;
     private final CommentPage commentPage;
     private final CommentWindow commentWindow;
 
     public Test22() {
-        super();
+        this.driver = new CommentsDriver();
         this.commentPage = new BCommentPage(this.driver);   /*why we  need it ?*/
         this.commentWindow = new BCommentWindow(this.driver);
     }
@@ -28,6 +30,17 @@ public class Test22 extends BaseDriver {
         commentWindow.saveComment();
         String errorText = commentWindow.showErrorMessage();
         assertEquals(errorText, "The Comment Text field should contain alphanumeric characters only");
+    }
+
+    @BeforeMethod
+    public void createDriver() {
+        this.driver.define();
+
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        this.driver.close();
     }
 
 }
