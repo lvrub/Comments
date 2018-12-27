34d34d08bf849fb8cf43004f6ac458ed5442fbfd
lvrub <leon_lm@i.ua>
2018-12-27 22:44:12 +0200

    Test improved

--- src/test/java/Tests/ft/Test2.java
+++ src/test/java/Tests/ft/Test2.java
@@ -10,23 +10,21 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Test2 {
-    protected final CommentsDriver driver;
-    protected final CommentPage commentPage;
-    protected final CommentTable commentTable;
-    protected final CommentWindow commentWindow;
+    protected final CommentsDriver driver = new CommentsDriver();
+    protected final CommentPage commentPage = new BCommentPage(driver);
+    protected final CommentTable commentTable = new BCommentTable(driver);
+    protected final CommentWindow commentWindow = new BCommentWindow(driver);
 
-    public Test2() {
-        this.driver = new CommentsDriver();
-        commentTable = new BCommentTable(driver);
-        commentPage = new BCommentPage(driver);
-        commentWindow = new BCommentWindow(driver);
+    @BeforeMethod
+    public void createDriver() throws ExceptionInInitializerError {
+        this.driver.define();
     }
-
     @Test(description = "Verify duplication of comment")
     @Description("This test verifies that uset is able to duplicate a comment")
     @Severity(SeverityLevel.NORMAL)
     @Epic("Regression Suit")
     @Feature("Duplication of a comment")
+
     public void test() {
         commentPage.open();
         commentTable.checkExistingComment(0);
@@ -45,11 +43,6 @@ public class Test2 {
     @AfterMethod
     public void closeDriver() {
         this.driver.quit();
-    }
-
-    @BeforeMethod
-    public void createDriver() {
-        this.driver.define();
     }
 
 }
