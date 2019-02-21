42edbccf53dfe311f45c610e8b0feabfb53b4960
lvrub <leon_lm@i.ua>
2019-02-21 23:19:52 +0200

    BasePages class added context methods are added

--- src/test/java/Tests/object/BasePages.java
+++ src/test/java/Tests/object/BasePages.java
@@ -1,10 +1,6 @@
 package Tests.object;
 
-import org.openqa.selenium.WebDriver;
-
 public class BasePages {
-
-    private WebDriver driver;
 
     public BCommentPage returnPageContext(BCommentPage commentPage) {
         return (commentPage);
