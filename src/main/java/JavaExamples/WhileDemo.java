1a9bce6beffc2743149dd9a7a919cbb4546291ca
lvrub <leon_lm@i.ua>
2019-02-27 22:38:46 +0200

    New java examples

--- src/main/java/JavaExamples/WhileDemo.java
+++ src/main/java/JavaExamples/WhileDemo.java
@@ -1,5 +1,8 @@
 package JavaExamples;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class WhileDemo {
     public static void main(String[] args) {
         int count = 1;
@@ -9,4 +12,41 @@ class WhileDemo {
         }
     }
 
+}
+
+class WhileDemoSelf {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        try {
+            int i = 0;
+            while (i <= 10) {
+                i++;
+                list.add(i);
+//                    System.out.println(" " + list);
+            }
+            System.out.println("Final" + list);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        int y = list.get(4);
+        System.out.println(y);
+
+        Integer number;
+        switch (y / 2) {
+            case 0:
+                number = y++;
+                break;
+            case 1:
+                number = y--;
+                break;
+            default:
+                number = 0;
+                break;
+        }
+        System.out.println(number);
+
+    }
 }
