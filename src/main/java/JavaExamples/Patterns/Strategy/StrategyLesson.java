c92365eef87e047dcc0430b29a0c370706bc348b
lvrub <leon_lm@i.ua>
2019-02-27 22:51:29 +0200

    New java examples

---
+++ src/main/java/JavaExamples/Patterns/Strategy/StrategyLesson.java
@@ -0,0 +1,47 @@
+package JavaExamples.Patterns.Strategy;
+
+interface Strategy {
+    double getPrice(double price);
+}
+
+public class StrategyLesson {
+    public static void main(String[] args) {
+        double startPrice = 100;
+        Strategy strategy_1 = new FullPrice();
+        Strategy strategy_2 = new HalfPrice();
+
+        ContextStrategy context_1 = new ContextStrategy(strategy_1);
+        double price_1 = context_1.getPrice(startPrice);
+        ContextStrategy context_2 = new ContextStrategy(strategy_2);
+        double price_2 = context_2.getPrice(startPrice);
+        System.out.println(price_1);
+        System.out.println(price_2);
+    }
+}
+
+class FullPrice implements Strategy {
+    @Override
+    public double getPrice(double startPrice) {
+        return startPrice;
+    }
+}
+
+class HalfPrice implements Strategy {
+    @Override
+    public double getPrice(double startPrice) {
+        return startPrice * 0.5;
+    }
+}
+
+class ContextStrategy {
+    Strategy strategy;
+
+    public ContextStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public double getPrice(double price) {
+        return strategy.getPrice(price);
+    }
+
+}
