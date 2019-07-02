505bfbbaa77693ef667f9142c15af7f0c386385d
lvrub <leon_lm@i.ua>
2019-07-02 22:02:10 +0300

    ENUM Collections example

---
+++ src/main/java/JavaExamples/Collections.java
@@ -0,0 +1,31 @@
+package JavaExamples;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class Collections {
+
+    public static void main(String[] arg) {
+
+        HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+
+        hmap.put(12, "Chaitanya");
+        hmap.put(2, "Rahul");
+        hmap.put(7, "Singh");
+        hmap.put(49, "Ajeet");
+        hmap.put(3, "Anuj");
+
+
+        Set set = hmap.entrySet();
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry mentry = (Map.Entry) iterator.next();
+            System.out.print("key is: " + mentry.getKey() + " & Value is: ");
+            System.out.println(mentry.getValue());
+        }
+
+
+    }
+}
