912ef1b80f88600f29b688757de31b89aa26182e
lvrub <leon_lm@i.ua>
2020-11-30 21:55:21 +0200

    fixed testNG

--- TestNGBatchFile.bat
+++ TestNGBatchFile.bat
@@ -1,5 +1,5 @@
 set projectLocation=C:\Automation
 cd %projectLocation%
-set classpath=%projectLocation%\bin
-set classpath=%projectLocation%\lib\*
-java -cp C:\ATTools\testngrunner\lib\*; %projectLocation% org.testng.TestNG testNg.xml
+set classpath=ATTools\bin
+set classpath=ATTools\lib\*
+java -cp C:\ATTools\testngrunner\lib\*; org.testng.TestNG testNg.xml
